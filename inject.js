725321f3871f2ed78ae81f1d9ff7b40c88f239ea
Joe Haines <hello@joehaines.co.uk>
2015-05-04 12:46:36 +0100

    Match names exactly

--- inject.js
+++ inject.js
@@ -1,9 +1,38 @@
 var app = {};
+console.info('twitch chat colours loaded');
 
-$(function(){
-    app.initialise = function() {
-        $.get('test.json', function(data) {
-            
-        }, 'json');
-    };
+$(function() {
+  app.people = [
+    {
+      'name': 'jkolea',
+      'colour': 'orange'
+    }
+  ];
+
+  app.colours = {
+    'orange': 'orange',
+    'red': '#ff0000'
+  };
+
+  app.initialise = function() {
+    console.info('initialise');
+
+    setInterval(function() {
+      app.doColouring(app.people);
+    }, 500);
+  };
+
+  app.doColouring = function(people) {
+    $(people).each(function() {
+      var self = this;
+
+      $('.from').filter(function() {
+        return this.innerHTML.toLowerCase() === self.name.toLowerCase();
+      }).css('color', app.colours[self.colour]);
+    });
+  };
+
+  // start app
+  console.info('start');
+  app.initialise();
 });
